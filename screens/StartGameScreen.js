0505c0d7708e2a7670dd57cf663022a3141f8ac3
Jim124 <fengpingan065@gmail.com>
2024-05-25 20:27:28 +0800

    add helper folder to manage global color

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,6 +1,7 @@
 import {View,TextInput,StyleSheet,Alert} from 'react-native';
 import PrimaryButton from '../components/PrimaryButton';
 import { useState } from 'react';
+import Colors from '../consts/colors';
 
 function StartGameScreen({onPickNumber}) {
     const [enteredNumber,setEnteredNumber] = useState('');
@@ -49,7 +50,7 @@ const styles = StyleSheet.create({
         padding: 16,
         borderRadius: 8,
         marginHorizontal: 24,
-        backgroundColor: '#3b021f',
+        backgroundColor: Colors.primary800,
         elevation: 4,
         shadowColor: "black",
         shadowOffset: {width:0,height:2},
@@ -62,9 +63,9 @@ const styles = StyleSheet.create({
         height: 50,
         width: 50,
         fontSize: 32,
-        borderBottomColor: '#ddb52f', 
+        borderBottomColor: Colors.accent500, 
         borderBottomWidth: 2,
-        color: '#ddb52f',
+        color: Colors.accent500,
         marginVertical: 8,
         fontWeight: 'bold',
         textAlign: 'center',
