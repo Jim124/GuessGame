cf02705434554d103fbf8869fb8763315576f1bc
Jim124 <fengpingan065@gmail.com>
2024-05-25 12:26:22 +0800

    use SafeAreaView component

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import {StyleSheet,ImageBackground} from 'react-native';
+import {StyleSheet,ImageBackground,SafeAreaView} from 'react-native';
 import {LinearGradient} from "expo-linear-gradient";
 import StartGameScreen from "./screens/StartGameScreen";
 import { useState } from 'react';
@@ -18,11 +18,13 @@ export default function App() {
   return (
     <LinearGradient colors={['#4e0329','#ddb52f']} style={styles.rootScreen}>
       <ImageBackground 
-      source={require('./assets/images/background.jpg')} 
-      resizeMode='cover'
-      style={styles.rootScreen}
-      imageStyle={styles.backgroundImage}>
-        {screen}
+        source={require('./assets/images/background.jpg')} 
+        resizeMode='cover'
+        style={styles.rootScreen}
+        imageStyle={styles.backgroundImage}>
+        <SafeAreaView style={styles.rootScreen}>
+          {screen}
+        </SafeAreaView>
       </ImageBackground>
     </LinearGradient>
   );
--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,10 +1,24 @@
-import {View,Text} from 'react-native'
+import {View,Text,StyleSheet} from 'react-native'
 function GameScreen() {
     return (
-        <View>
-            <Text>GameScreen</Text>
+        <View style={styles.screen}>
+            <Text>Opponent's Guess</Text>
+            {/* GUESS  */}
+            <View>
+                <Text>Higher or Lower?</Text>
+                {/* +
+                - */}
+            </View>
+            {/* <View>LOG ROUNDS</View> */}
         </View>
     )
 }
 
 export default GameScreen
+
+const styles = StyleSheet.create({
+    screen:{
+        flex: 1,
+        padding: 12,
+    }
+});
